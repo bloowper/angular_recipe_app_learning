9273c8baf07f83e622b68392fadd9a9770ca8230
tomek <tomasz.orchowski@stazysta.comarch.pl>
2021-07-26 10:38:28 +0200

    .

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,9 +9,9 @@ import {Recipe} from "./recipe.model";
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
     new Recipe("Sample recipe one","\n" +
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ante nibh, accumsan ac dignissim eu, porttitor et lacus. In eget leo a nunc varius rhoncus. In eget nulla eu lectus bibendum semper. Proin rhoncus vulputate placerat. Etiam nisi metus, pretium non ullamcorper at, auctor eget enim. Quisque dictum, tellus in commodo elementum, ante velit auctor erat, sit amet aliquet metus orci tempus lectus. Donec blandit neque ac dui ultricies iaculis.","https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2015/11/shakshuka-11.jpg"),
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ante nibh, accumsan ac dignissim ","https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2015/11/shakshuka-11.jpg"),
     new Recipe("The second sample","\n" +
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ante nibh, accumsan ac dignissim eu, porttitor et lacus. In eget leo a nunc varius rhoncus. In eget nulla eu lectus bibendum semper. Proin rhoncus vulputate placerat. Etiam nisi metus, pretium non ullamcorper at, auctor eget enim. Quisque dictum, tellus in commodo elementum, ante velit auctor erat, sit amet aliquet metus orci tempus lectus. Donec blandit neque ac dui ultricies iaculis.","https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2015/11/shakshuka-11.jpg")
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ante nibh, accumsan ac dignissim ","https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2015/11/shakshuka-11.jpg")
 
   ];
   constructor() { }
