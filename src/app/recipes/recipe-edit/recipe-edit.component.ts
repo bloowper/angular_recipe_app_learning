9bf738c2b19c3b93fa080f4431485ac102e85cae
tomek <tomasz.orchowski@stazysta.comarch.pl>
2021-08-04 15:20:47 +0200

    .

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -67,6 +67,7 @@ export class RecipeEditComponent implements OnInit {
     onAddIngredient() {
         (<FormArray>this.recipeForm.get('ingredients')).push(new FormGroup({
             'name': new FormControl(),
+
             'amount': new FormControl()
         }))
     }
