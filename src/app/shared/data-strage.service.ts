9b46c265bd45a48fcbdd5173d3d4c45946ee5c77
bloowper <bloowper@gmail.com>
2021-08-07 19:26:05 +0200

    .

--- src/app/shared/data-strage.service.ts
+++ src/app/shared/data-strage.service.ts
@@ -1,6 +1,10 @@
 import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
 import {RecipeService} from "../recipes/recipe.service";
+<<<<<<< HEAD
+=======
+import {Recipe} from "../recipes/recipe-list/recipe.model";
+>>>>>>> 56681a0... recipe edit/ afte deleting by git
 
 @Injectable({
     providedIn: 'root'
@@ -22,8 +26,15 @@ export  class  DataStorageService{
     }
 
     fetchRecipes() {
+<<<<<<< HEAD
         this.httpClient.get("https://recipe-app-test-ae9dd-default-rtdb.firebaseio.com/recipes.json").subscribe(value => {
             console.log(value)
         })
+=======
+        this.httpClient.get<Recipe[]>("https://recipe-app-test-ae9dd-default-rtdb.firebaseio.com/recipes.json").subscribe(value => {
+            this.recipeService.setRecipes((value));
+        })
+
+>>>>>>> 56681a0... recipe edit/ afte deleting by git
     }
 }
