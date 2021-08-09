c9d0ee6bbe0ac548f44eb1a5dc0ce5ffe6c9d1e9
bloowper <bloowper@gmail.com>
2021-08-09 21:10:20 +0200

    .

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from "@angular/core";
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpErrorResponse} from "@angular/common/http";
 import {catchError} from "rxjs/operators";
 import {throwError} from "rxjs";
 
@@ -28,44 +28,51 @@ export class AuthService{
     }
 
     signup(email: string, password: string) {
-        return this.httpClient.
-        post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBKklu_He8ReMEJsIrhkG61IpOX8HMbdPY',
+        return this.httpClient.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBKklu_He8ReMEJsIrhkG61IpOX8HMbdPY',
             {
                 email: email,
                 password: password,
                 returnSecureToken: true
-            }).
-        pipe(catchError(error => {
-            let errorMessage = "an unknown error has occurred"
-            if (!error.error || !error.error.error) {
-                return throwError(errorMessage);
             }
-            switch (error.error.error.message) {
-                case 'EMAIL_EXISTS':
-                    errorMessage = "Email exist";
-                    break;
-            }
-            return throwError(errorMessage);
-        }));
+        ).
+        pipe(catchError(this.handleError));
     }
 
     login(email: string, password: string) {
         return this.httpClient.post<AuthResponseData>("https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBKklu_He8ReMEJsIrhkG61IpOX8HMbdPY",
-            {
+        {
                 email: email,
                 password: password,
                 returnSecureToken: true
             }
-            );
+        ).
+        pipe(catchError(this.handleError));
     }
 
-
+    private handleError(errorResponse: HttpErrorResponse) {
+        let errorMessage = "an unknown error has occurred"
+        if (!errorResponse.error || !errorResponse.error.error) {
+            return throwError(errorMessage);
+        }
+        switch (errorResponse.error.error.message) {
+            case 'EMAIL_EXISTS':
+                errorMessage = "Email exist";
+                break;
+            case 'EMAIL_NOT_FOUND':
+                errorMessage = "Email not found";
+                break;
+            case 'INVALID_PASSWORD':
+                errorMessage = "Password in invalid";
+                break;
+            case 'USER_DISABLED':
+                errorMessage = "User is disabled by administrator";
+                break;
+        }
+        return throwError(errorMessage);
+    }
 }
 
 
-// idToken	string	A Firebase Auth ID token for the authenticated user.
-//     email	string	The email for the authenticated user.
-//     refreshToken	string	A Firebase Auth refresh token for the authenticated user.
-//     expiresIn	string	The number of seconds in which the ID token expires.
-//     localId	string	The uid of the authenticated user.
-//     registered	boolean	Whether the email is for an existing account.
+//     EMAIL_NOT_FOUND: There is no user record corresponding to this identifier. The user may have been deleted.
+//     INVALID_PASSWORD: The password is invalid or the user does not have a password.
+//     USER_DISABLED: The user account has been disabled by an administrator.
